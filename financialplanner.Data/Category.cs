167deb22506fd853c17af3e7d675f4e0b8a50c6f
dariiaivanova <dashadangerous@gmail.com>
2016-05-22 14:39:39 +0300

    added Category class, tried to make a tile view of Categories

---
+++ financialplanner.Data/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace financialplanner.Data
+{
+   public class Category
+    {
+        public ImageSource Image { get; set; }
+        public string Name { get; set; }
+
+        public Category Parent { get; set; }
+
+        public Category(ImageSource image, string name)
+        {
+            Image = image;
+            Name = name;
+        }
+    }
+}
---
+++ finplanner.UI/PageCategories.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using financialplanner.Data;
+
+namespace finplanner.UI
+{
+    /// <summary>
+    /// Interaction logic for PageCategories.xaml
+    /// </summary>
+    public partial class PageCategories : Page
+    {
+        public PageCategories()
+        {
+            InitializeComponent();
+            Categories = new ObservableCollection<Category>
+            {
+                new Category(GetImageFromResourceString("beach"), "beach"),
+                new Category(GetImageFromResourceString("car"), "car"),
+                new Category(GetImageFromResourceString("car"), "car")
+            };
+            ListBox.ItemsSource = Categories;
+
+        }
+
+        private void TabItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        public ObservableCollection<Category> Categories { get; set; }
+        private static BitmapImage GetImageFromResourceString(string imageName)
+        {
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri("Resources\\"+imageName + ".png", UriKind.Relative);
+            image.EndInit();
+            return image;
+        }
+
+    }
+}
