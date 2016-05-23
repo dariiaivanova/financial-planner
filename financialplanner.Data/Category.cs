fc4ce94b1a2fabad540aa0e8b9c34265cd7e388e
dariiaivanova <dashadangerous@gmail.com>
2016-05-24 00:39:42 +0300

    added list of Children into Category

--- financialplanner.Data/Category.cs
+++ financialplanner.Data/Category.cs
@@ -7,17 +7,36 @@ using System.Windows.Media;
 
 namespace financialplanner.Data
 {
-   public class Category
+    public class Category
     {
-        public ImageSource Image { get; set; }
-        public string Name { get; set; }
+        public ImageSource Image { get; private set; }
+        public string Name { get; private set; }
+        public Category Parent { get; private set; }
 
-        public Category Parent { get; set; }
+        public IList<Category> Children { get; private set; }
 
         public Category(ImageSource image, string name)
+            : this(name)
         {
             Image = image;
+        }
+        
+        public Category(Category parent, string name)
+            : this(name)
+        {
+            Parent = parent;
+            Parent.Children.Add(this);
+        }
+
+        private Category(string name)
+        {
             Name = name;
+            Children = new List<Category>();
+        }
+
+        public bool IsRootNode()
+        {
+            return Parent == null;
         }
     }
 }
